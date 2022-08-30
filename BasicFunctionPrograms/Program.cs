6846530a49a9205772698db1afa65dd0b82735df
navitha <navithareddy112@gmail.com>
2022-08-30 21:38:27 +0530

    [navitha] 6.RemainderAndQuotient

--- BasicFunctionPrograms/Program.cs
+++ BasicFunctionPrograms/Program.cs
@@ -6,7 +6,7 @@
         {
             //Console.WriteLine("Hello, World!");
             Console.WriteLine("welcome to function programming");
-            Console.WriteLine("choose option\n 1.flipcoin \t 2.Leap year \t 3. power of two \t 4. Harmonic number \t 5.Primefactor \t 6.Quotient And Remainder \t 7. swap \t 8. Even  Or Odd \t 9. Vowel Consonent \t 10. largest three numbers");
+            Console.WriteLine("choose option\n 1.flipcoin \t 2.Leap year \t 3. power of two \t 4. Harmonic number \t 5.Primenumber \t 6.Quotient And Remainder \t 7. swap \t 8. Even  Or Odd \t 9. Vowel Consonent \t 10. largest three numbers");
             int option = Convert.ToInt32(Console.ReadLine());
             switch (option)
             {
@@ -23,7 +23,13 @@
                     HarmonicValue.HormonicNumber();
                     break;
                 case 5:
-                    PrimeNumberFactor.PrimeFactorNumber();
+                    PrimeNumber.PrimeFactorNumber();
+                    break;
+                case 6:
+                    RemainderAndQuotient.QuatientAndRemainderNumber();
+                    break;
+                case 7:
+                    SwapNumbers.SwapTwoNumbers();
                     break;
 
             }
