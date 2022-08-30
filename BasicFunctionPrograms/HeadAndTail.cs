4fb28d183e5b3d9dd214feebcda2d061ad7ae360
navitha <navithareddy112@gmail.com>
2022-08-30 21:01:53 +0530

    [navitha] 1.HeadAndTail

---
+++ BasicFunctionPrograms/HeadAndTail.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicFunctionPrograms
+{
+    internal class HeadAndTail
+    {
+        public static void CalculatePercentage()
+        {
+            int headcount = 0, tailcount = 0;
+            double headpercentage;
+            double tailpercentage;
+            Console.WriteLine("please enter a value number of flips ypu want");
+            int flipcoin = Convert.ToInt32(Console.ReadLine());
+
+            if (flipcoin > 0)
+            {
+                for (int i = 0; i < flipcoin; i++)
+                {
+                    Random random = new Random();
+                    double number = random.NextDouble();  //0,1
+                    if (number > 0.5)
+                    {
+                        Console.WriteLine("head");
+                        headcount++;
+
+                    }
+                    else
+                    {
+                        Console.WriteLine("tail");
+                        tailcount++;
+                    }
+                }
+                headpercentage = headcount * 100 / flipcoin;
+                tailpercentage = tailcount * 100 / flipcoin;
+                Console.WriteLine("headpercentage " + headpercentage + "tailpercentage" + tailpercentage);
+            }
+            else
+            {
+                Console.WriteLine("Enter wrong choice");
+            }
+
+        }
+
+    }
+}
---
+++ BasicFunctionPrograms/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace BasicFunctionPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("welcome to function programming");
+            Console.WriteLine("choose option\n 1.flipcoin \t 2.Leap year \t 3. power of two \t 4. Harmonic number \t 5.Primefactor \t 6.Quotient And Remainder \t 7. swap \t 8. Even  Or Odd \t 9. Vowel Consonent \t 10. largest three numbers");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    HeadAndTail.CalculatePercentage();
+                    break;
+            }
+        }
+    }
+}
